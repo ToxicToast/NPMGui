fd658bde050a5782271833b8db264b1b5b76b908
Thomas Kortyka <tkortyka@alegri.eu>
2019-01-11 16:16:31 +0100

    added material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,29 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
 
-const materialModules = [];
+const materialModules = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatGridListModule,
+  MatListModule,
+  MatButtonModule,
+  MatChipsModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatTableModule
+];
 
 @NgModule({
   declarations: [],
